6d261ee77fa702e64649200751d6bef1e3762d74
Connor Abdelnoor & Tanner Heffner <student@epicodus.com>
2015-03-12 10:01:15 -0700

    creates new test for scoring of multiple 1 point letters.

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -3,7 +3,10 @@
     {
         function scoreWord($letters)
         {
-
+            $one_pointers = array('A', 'E', 'I', 'O', 'U', 'L','N', 'R', 'S', 'T');
+            if(in_array(strtoupper($letters), $one_pointers)){
+                return 1;
+            }
         }
     }
 
--- tests/ScrabbleTest.php
+++ tests/ScrabbleTest.php
@@ -2,7 +2,7 @@
 
     require_once "src/Scrabble.php";
 
-    class WordTest extends PHPUnit_FrameWork_TestCase
+    class WordTest extends PHPUnit_Framework_TestCase
     {
         function test_scoreWord_1letter()
         {
@@ -15,6 +15,33 @@
 
             //Assert
             $this->assertEquals(1, $result);
+        }
+
+        function test_scoreWord_1pointers()
+        {
+            //Arrange
+            $test_Scrabble = new Word;
+            // input is any one of the 1 point letters
+            $input1 = "t";
+
+            //Act
+            $result = $test_Scrabble->scoreWord($input1);
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
+        function test_scoreWord_2letter2Points()
+        {
+            //Arrange
+            $test_Scrabble = new Word;
+            $input1 = "at";
+
+            //Act
+            $result = $test_Scrabble->scoreWord($input1);
+
+            //Assert
+            $this->assertEquals(2, $result);
         }
     }
 
